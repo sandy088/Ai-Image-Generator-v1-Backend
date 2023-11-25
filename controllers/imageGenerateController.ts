d023cf1b3501440c6c296a0bba10d08c4e835f7a
Sandeep <sandeeppakho55@gmail.com>
2023-11-25 21:49:30 +0530

    Bug Fixes: Minor

--- controllers/imageGenerateController.ts
+++ controllers/imageGenerateController.ts
@@ -4,37 +4,44 @@ import upscaleImg from "../utils/ImageGenerationUtils/imageGenerate";
 import dotenv from "dotenv";
 dotenv.config();
 const generateImage = async (req: Request, res: Response) => {
-    const {prompt} = req.body;
+    try {
+        const { prompt } = req.body;
+        
+        const token = req.headers.authorization?.split(" ")[1];
 
-    const token = req.headers.authorization?.split(" ")[1];
+        //validations
+        if (!prompt || prompt === "") {
+            return res.status(400).json({
+                message: "Prompt is required"
+            })
 
-    //validations
-    if(!prompt){
-        res.status(400).json({
-            message: "Prompt is required"
-        })
-    
-    }
+        }
 
-    if(!token || token !== process.env.API_TOKEN){
-        res.status(401).json({
-            message: "Invalid Token"
-        })
-    }
+        if (!token || token !== process.env.API_TOKEN) {
+            return res.status(401).json({
+                message: "Invalid Token"
+            })
+        }
 
-    const output: { error?: string } = await upscaleImg(prompt);
+        const output: { error?: string } = await upscaleImg(prompt);
 
-    if (output.error) {
-        res.status(500).json({
+        if (output.error) {
+            return res.status(500).json({
+                message: "Error in generating image",
+                error: output.error
+            });
+        }
+
+        return res.status(200).json({
+            message: "Image generated",
+            output: output
+        });
+    } catch (error) {
+        return res.status(500).json({
             message: "Error in generating image",
-            error: output.error
+            error: error
         });
     }
-
-    res.status(200).json({
-        message: "Image generated",
-        output: output
-    });
 }
 
 export default [generateImage]
